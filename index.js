b929bcd347d9094356763be42378a7f50906fd50
BBoltzmann <woleoshoifeoluwa@gmail.com>
2022-07-16 21:02:47 +0100

    aje website

---
+++ index.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById('hamburger');
+const navbar = document.getElementById('navbar');
+const storeIcon = document.getElementById('storeIcon');
+
+hamburger.addEventListener('click', () => {
+
+
+})
+
+function show() {
+    document.getElementById('navbar1').style.display = 'flex';
+}
